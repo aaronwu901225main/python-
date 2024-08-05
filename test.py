d804770d5d0f3a5cb362e1add47db77fff3ad1cc
aaronwu901225main <aaronwu901225main@gmail.com>
2024-08-05 16:14:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-a=[1,2,3]
-b=[4,5,6]
-print(a+b)
+# 示例列表
+example_list = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+
+# 创建空字典
+frequency_dict = {}
+
+# 遍历列表并计数
+for item in example_list:
+    if item in frequency_dict:
+        frequency_dict[item] += 1
+    else:
+        frequency_dict[item] = 1
+
+# 打印结果
+print(frequency_dict)
