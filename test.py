7044c6de841e0615f2cb635c1ad310d08849ce79
aaronwu901225main <aaronwu901225main@gmail.com>
2024-08-09 22:14:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-a=[1,2,2,3,4,8]+[8]
-print(a)
+nums = [1,2,2,2,2,4]
+index_to_remove = 2  # 想删除的位置
+
+removed_element = nums.pop()
+
+print(nums)  # 输出: [1, 2, 4, 5, 6]
+print(removed_element)  # 输出: 3
