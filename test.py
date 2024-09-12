656e5640f33c7a3afc82e0bcbb8b79790cc0ffa8
aaronwu901225main <aaronwu901225main@gmail.com>
2024-09-12 17:18:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-# 定義 Sans 的臉部特徵
-face = np.array([
-    [0, 0, 1, 1, 1, 1, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1, 0],
-    [1, 1, 0, 1, 1, 0, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [0, 1, 1, 1, 1, 1, 1, 0],
-    [0, 0, 1, 1, 1, 1, 0, 0]
-])
-
-# 繪製 Sans 的臉
-plt.imshow(face, cmap='gray')
-plt.axis('off')
-plt.show()
+n=8
+current=[a+1 for a in range(n)]
+current.pop(2)
+print(current)
