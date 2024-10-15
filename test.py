b034f9ec03ead5b1fa2d782d74d7a96b0d6672cf
aaronwu901225main <aaronwu901225main@gmail.com>
2024-10-15 16:12:45 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,13 +2,12 @@ from datasets import load_dataset
 
 # 加载 CSV 文件到 Hugging Face Dataset
 data_files = {
-    "train": "C:\\Users\\AaronWu\\Desktop\\folder\\暫存\\titanic\\train.csv",
-    "test": "C:\\Users\\AaronWu\\Desktop\\folder\\暫存\\titanic\\test.csv"
+    "train": "C:\\Users\\AaronWu\\Desktop\\xcat.zip",
+    "test": "C:\\Users\\AaronWu\\Desktop\\xcat test result.zip"
 }
 
 # 加载数据集
-dataset = load_dataset('csv', data_files=data_files)
+dataset = load_dataset('zip', data_files=data_files)
 
 # 将数据集推送到 Hugging Face Hub
-dataset.push_to_hub("dataset")
-
+dataset.push_to_hub("test")
