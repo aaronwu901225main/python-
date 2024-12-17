ae4a2a59ee17915c8742988ba74d021d67fa8f4a
aaronwu901225main <aaronwu901225main@gmail.com>
2024-12-17 20:28:21 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-import torch
-print(torch.cuda.is_available())
-print(torch.__version__)
+early_stop_patience = 10  # 早期停止的耐心值
+best_val_loss = float('inf')  # 記錄最佳驗證損失
+early_stop_counter = 0  # 計算未改善的回合數
