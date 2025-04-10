1737cb98922b8aef5b1d9050b2dcd5831188ad41
aaronwu901225main <aaronwu901225main@gmail.com>
2025-04-10 16:34:12 +0800

    Delete tempCodeRunnerFile.py

--- Computer_vision_hw/Vanish point/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-        max_width = 1280
-        max_height = 720
-        h, w = img.shape[:2]
-        scale = min(max_width / w, max_height / h)
-        new_size = (int(w * scale), int(h * scale))
-        resized = cv2.resize(img, new_size)
